fae4d9e6fc0e2d5fcfb8d7828371f23fd46aad79
ashleyyyltr <ashleyyyltr@noreply.gitcode.com>
2025-07-31 13:10:34 +0800

    修改ioc匹配代码

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -325,7 +325,7 @@ void MPU_Config(void)
   MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
   MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
   MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;//MPU_ACCESS_NOT_BUFFERABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
 
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
   /* Enables the MPU */
